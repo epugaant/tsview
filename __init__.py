b844ba4866da11d5565f852795c2b47b29ae47a1
elenapuga <epuga@sciops.esa.int>
2023-08-28 09:32:51 +0200

    commit initial tests, requirements and __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+'''tsview is a web application to visualize time series data products served by ESA Archives APIs'''
+import os
+
+__version__ = '0.1'
+
+
+# set Python env variable to keep track of example data dir (e.g orbitize)
+data_dir = os.path.dirname(__file__)
+DATADIR = os.path.join(data_dir, 'example_data/')
+
+#methods that the interpreter will use in the wild card import statement
+# __all__ = ["attitude",
+#            "utils"]
---
+++ tests/io_test.py
@@ -0,0 +1,13 @@
+import pytest
+import numpy as np
+from astropy import units as u
+
+from tsview.io import ts_fits_reader, DATADIR
+
+def test_fits_parser():
+    # test #1 loading x1dints
+    # test #2 loading rateints
+    pass
+
+if __name__ == '__main__':
+    #test_fits_parser()
